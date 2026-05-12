1487a95edd87c02376260b00c55f678624003de4
russell romney <russellromney@gmail.com>
2026-05-12 15:18:36 -0700

    Update call_blocking tests for call context

--- tina-runtime/tests/threaded_call_blocking.rs
+++ tina-runtime/tests/threaded_call_blocking.rs
@@ -2,8 +2,9 @@ use std::convert::Infallible;
 use std::time::Duration;
 
 use tina::prelude::*;
+use tina::{CallContext, RequestContext, reply_to_request};
 use tina_runtime::{
-    CallOutcome, DefaultThreadedMailboxFactory, ThreadedRuntime, ThreadedRuntimeConfig,
+    CallOutcome, DefaultThreadedMailboxFactory, RuntimeCall, ThreadedRuntime, ThreadedRuntimeConfig,
 };
 
 #[derive(Debug, Clone, Copy)]
@@ -22,11 +23,24 @@ enum EchoMsg {
 
 struct Echo;
 
-#[tina_runtime::isolate(message = EchoMsg, reply = u32, shard = TestShard)]
-impl Echo {
+impl Isolate for Echo {
+    type Message = EchoMsg;
+    type Reply = u32;
+    type Send = Outbound<Infallible>;
+    type Spawn = Infallible;
+    type SpawnObserved = Infallible;
+    type Call = Infallible;
+    type Shard = TestShard;
+
     fn handle(&mut self, msg: EchoMsg, _ctx: &mut Context<'_, TestShard, u32>) -> Effect<Self> {
         match msg {
             EchoMsg::AddOne(value) => reply(value + 1),
+        }
+    }
+
+    fn handle_call(&mut self, msg: EchoMsg, call: CallContext<'_, Self>) -> Effect<Self> {
+        match msg {
+            EchoMsg::AddOne(value) => call.reply(value + 1),
         }
     }
 }
@@ -34,15 +48,34 @@ impl Echo {
 #[derive(Debug)]
 enum SilentMsg {
     Start,
+    Done(RequestContext<u32>),
 }
 
 struct Silent;
 
-#[tina_runtime::isolate(message = SilentMsg, reply = u32, shard = TestShard)]
-impl Silent {
+impl Isolate for Silent {
+    type Message = SilentMsg;
+    type Reply = u32;
+    type Send = Outbound<Infallible>;
+    type Spawn = Infallible;
+    type SpawnObserved = Infallible;
+    type Call = RuntimeCall<SilentMsg>;
+    type Shard = TestShard;
+
     fn handle(&mut self, msg: SilentMsg, _ctx: &mut Context<'_, TestShard, u32>) -> Effect<Self> {
         match msg {
             SilentMsg::Start => noop(),
+            SilentMsg::Done(req) => reply_to_request(req, 1),
+        }
+    }
+
+    fn handle_call(&mut self, msg: SilentMsg, call: CallContext<'_, Self>) -> Effect<Self> {
+        match msg {
+            SilentMsg::Start => {
+                let req = call.into_request_context();
+                tina_runtime::sleep(Duration::from_millis(100)).reply(move |_| SilentMsg::Done(req))
+            }
+            SilentMsg::Done(_) => call.reject(tina::CallRejectedReason::UnsupportedMessage),
         }
     }
 }
@@ -55,12 +88,26 @@ enum StopperMsg {
 
 struct Stopper;
 
-#[tina_runtime::isolate(message = StopperMsg, reply = u32, shard = TestShard)]
-impl Stopper {
+impl Isolate for Stopper {
+    type Message = StopperMsg;
+    type Reply = u32;
+    type Send = Outbound<Infallible>;
+    type Spawn = Infallible;
+    type SpawnObserved = Infallible;
+    type Call = Infallible;
+    type Shard = TestShard;
+
     fn handle(&mut self, msg: StopperMsg, _ctx: &mut Context<'_, TestShard, u32>) -> Effect<Self> {
         match msg {
             StopperMsg::Stop => stop(),
             StopperMsg::Ping => reply(1),
+        }
+    }
+
+    fn handle_call(&mut self, msg: StopperMsg, call: CallContext<'_, Self>) -> Effect<Self> {
+        match msg {
+            StopperMsg::Ping => call.reply(1),
+            StopperMsg::Stop => call.reject(tina::CallRejectedReason::UnsupportedMessage),
         }
     }
 }
